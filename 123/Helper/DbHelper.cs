34cd518c77ccf3b3b2723bc3da754b7b19a046c5
Amin <aminheidari85@agmail.com>
2018-08-10 15:23:31 +1000

    Importing project

---
+++ 123/Helper/DbHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Database;
+using Android.Database.Sqlite;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Application.Helper
+{
+    public class DbHelper : SQLiteOpenHelper
+    {
+        private static string DB_NAME = "ToDoList";
+        private static int DB_VER = 1;
+        public static string DB_TABLE = "Task";
+        public static string DB_COLUMN = "TaskName";
+
+        public DbHelper(Context context) :base(context,DB_NAME,null,DB_VER) { }
+        public override void OnCreate(SQLiteDatabase db)
+        {
+            string query = $"create table {DbHelper.DB_TABLE} (ID INTEGER PRIMARY KEY AUTOINCERMENT,{DbHelper.DB_COLUMN} TEXT NOT NULL);";
+            db.ExecSQL(query);
+        }
+
+        public override void OnUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+        {
+            string query = $"DELETE TABLE IF EXISTS {DB_TABLE}";
+            db.ExecSQL(query);
+            OnCreate(db);
+        }
+
+        public void InsertNewTask (string task) 
+        {
+            SQLiteDatabase db = this.WritableDatabase;
+            ContentValues values = new ContentValues();
+            values.Put(DB_COLUMN, task);
+            db.InsertWithOnConflict(DB_TABLE,null,values,Android.Database.Sqlite.Conflict.Replace);
+            db.Close();
+        }
+
+        public void deleteTask (string task) 
+        {
+            SQLiteDatabase db = this.WritableDatabase;
+            db.Delete(DB_TABLE,DB_COLUMN+ " = ? ",new string[] { task });
+            db.Close();
+        }
+
+        public List<string> getTaskList()
+        {
+            List<string> taskList = new List<string>();
+            SQLiteDatabase db = this.ReadableDatabase;
+            ICursor cursor = db.Query(DB_TABLE, new string[] { DB_COLUMN }, null, null, null, null, null);
+            while (cursor.MoveToNext())
+            {
+                int index = cursor.GetColumnIndex(DB_COLUMN);
+                taskList.Add(cursor.GetString(index));
+            }
+            return taskList;
+        }
+    }
+}
---
+++ 123/MainActivity.cs
@@ -0,0 +1,86 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.AppCompat;
+using Android.Views;
+using Android.Content;
+using System;
+using Application.Helper;
+using System.Collections.Generic;
+
+namespace Application
+{
+    [Activity(Label = "123", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        EditText taskEditText;
+        DbHelper dbHelper;
+        CustomAdapter adapter;
+        ListView firstTask;
+
+
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_item,menu);
+
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch(item.ItemId)
+            {
+                case Resource.Id.action_add:
+                    taskEditText = new EditText(this);
+                    Android.Support.V7.App.AlertDialog dialog = new Android.Support.V7.App.AlertDialog.Builder(this)
+                        .SetTitle("Add New Task")
+                        .SetMessage("What do you want to do next?")
+                        .SetView(taskEditText)
+                        .SetPositiveButton("Add", OkAction)
+                        .SetNegativeButton("Cancel", CancelAction)
+                        .Create();
+                    dialog.Show();
+                    return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void CancelAction(object sender, DialogClickEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OkAction(object sender, DialogClickEventArgs e)
+        {
+            string task = taskEditText.Text;
+            dbHelper.InsertNewTask(task);
+            LoadTaskList();
+        }
+
+        public void LoadTaskList()
+        {
+            List<string> taskList = dbHelper.getTaskList();
+            adapter = new CustomAdapter(this, taskList, dbHelper);
+            firstTask.Adapter = adapter;
+
+
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView (Resource.Layout.Main);
+
+            dbHelper = new DbHelper(this);
+            firstTask = FindViewById<ListView>(Resource.Id.firstTask);
+
+
+            // load data 
+
+            LoadTaskList();
+        }
+    }
+}
+
